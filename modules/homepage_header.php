b9ed0efff22e07f0e34a25e2039283680bf528f8
nodenbro <76056030+nodenbro@users.noreply.github.com>
2023-08-01 10:12:01 +0200

    Added category dropdown in homepage header

--- modules/homepage_header.php
+++ modules/homepage_header.php
@@ -30,6 +30,18 @@ if (get_row_layout() === 'homepage_header') {
      <div class="input-container">
             <form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
                 <input type="search" class="form-control" placeholder="Search" value="<?php echo get_search_query() ?>" name="s" title="Search" />
+                <?php
+                    wp_dropdown_categories( array(
+                    'show_option_all' => 'All Businesses',
+                    'orderby' => 'name',
+                    'echo' => 3,
+                    'selected' => $cat,
+                    'hierarchial' => true,
+                    'class' => 'location-dropdown',
+                    'id' => 'custom_loc_drop',
+                    'value_field' => 'term_id'
+                    ) );
+                ?>
                 <button type="submit" class="icon-search"><i class="fa fa-search"></i></button>
             </form>
     </div>
