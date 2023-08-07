f236123df9417b3a12bd15fd57b210475b8ed008
Almedin Agushi <awesome.coding27@gmail.com>
2023-08-07 10:23:42 +0200

    mouldes cards scrolling left and right

--- modules/modules_cards.php
+++ modules/modules_cards.php
@@ -1,23 +1,24 @@
 
-
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
 <div class="container">
     <?php
     $post_type_selected = get_sub_field('post_type_select');
     ?>
-    <h2>Cards Module</h2>
+
+    <h2 class="title-cards">Cards Module</h2>
 
     <?php if ($post_type_selected === 'pages'): ?>
         <?php $selected_pages = get_sub_field('pages'); ?>
         <?php if (!empty($selected_pages)): ?>
-            <div class="modules-cards-container">
+            <div class="modules-cards-container slider">
                 <?php foreach ($selected_pages as $page): ?>
                     <div class="card_page">
                         <div class="modules-card-image">
                             <?php echo get_the_post_thumbnail($page->ID, 'thumbnail'); ?>
                         </div>
                         <h3><?php echo get_the_title($page->ID); ?></h3>
-                       
                         <button class="btn-button"><a href="<?php echo get_permalink($page->ID); ?>">View Page</a></button>
                     </div>
                 <?php endforeach; ?>
@@ -26,7 +27,7 @@
     <?php elseif ($post_type_selected === 'businesses'): ?>
         <?php $selected_businesses = get_sub_field('businesses_test'); ?>
         <?php if (!empty($selected_businesses)): ?>
-            <div class="modules-cards-container">
+            <div class="modules-cards-container slider">
                 <?php foreach ($selected_businesses as $business): ?>
                     <div class="card_businesses">
                         <div class="modules-card-image">
@@ -43,20 +44,19 @@
 
 
 
-<style>
-
-
-
-
-
-
-
-
-</style>
-
-
-
-
+<script>
+        jQuery(document).ready(function ($) {
+            $('.modules-cards-container.slider').slick({
+               
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                prevArrow: '<button class="slick-prev "></button>',
+                nextArrow: '<button class="slick-next "></button>',
+                
+            });
+        });
+    </script>
 
 
 
