7457c6c7a2ff2bf8edd5fae321ac58c4b6e2fff3
Almedin Agushi <awesome.coding27@gmail.com>
2023-08-11 12:10:14 +0200

    cards

--- acf-export/acf-data.php
+++ acf-export/acf-data.php
@@ -247,7 +247,7 @@ acf_add_local_field_group(array(
 								'id' => '',
 							),
 							'default_value' => '',
-							'placeholder' => 'title...!',
+							'placeholder' => 'Title',
 							'prepend' => '',
 							'append' => '',
 							'maxlength' => '',
