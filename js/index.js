e6c15b704adbb33c5eb60df06b4447ad120ce403
Melisako <120452771+Melisako@users.noreply.github.com>
2023-08-11 11:49:16 +0200

    js-commit

--- js/index.js
+++ js/index.js
@@ -1,40 +1,33 @@
 
-
 jQuery(function($) {
-    var page = 2;
-    $('body').on('click', '.loadmore', function() {
+  var page = 2;
+  $('body').on('click', '.loadmore', function() {
+    const postDivs = document.querySelectorAll('.post');
+    var businessIds = document.getElementById('business_ids').value;
+        const dataIds = [];
+            postDivs.forEach((div) => {
+            const dataId = div.getAttribute('data-id');
+            if (dataId) {
+                dataIds.push(dataId);
+            }
+            });
+
         var data = {
             'action': 'load_posts_by_ajax',
             'page': page,
+            'displayed_businesses' : dataIds.join(','),
+            'allowedIds' : businessIds,
             'security': blog.security
         };
+      
 
-        $.post(blog.ajaxurl, data, function(response) {
-            if ($.trim(response) != '') {
-                $('.blog-posts').append(response);
-                page++;
-            } else {
-                $('.loadmore').hide();
-            }
-        });
-    });
+      $.post(blog.ajaxurl, data, function(response) {
+          if ($.trim(response) != '') {
+              $('.blog-posts').append(response);
+              page++;
+          } else {
+              $('.loadmore').hide();
+          }
+      });
+  });
 });
-  
-
-  
-    function showSuccessPopup(message, seconds) {
-        var popup = $('#successPopup');
-        var popupMessage = $('#successMessage');
-
-     
-        popupMessage.html(message);
-
-        
-        popup.show();
-
-       
-        setTimeout(function () {
-            popup.hide();
-        }, seconds * 1000); 
-    }
-
