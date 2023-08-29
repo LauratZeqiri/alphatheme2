568b7c0773b9e9951f161b289a3b3ada3b7fc4c7
LauratZeqiri <128151479+LauratZeqiri@users.noreply.github.com>
2023-08-29 22:29:17 +0200

    alpha

--- header.php
+++ header.php
@@ -12,24 +12,38 @@
 </head>
 <body <?php body_class() ?> >
 <section class="header">
-    
-<div class="container">
-<div class="img">
-    <a href="<?php echo get_home_url(); ?>">
-        <img src="<?php echo get_stylesheet_directory_uri() . '/img/foto1.svg'; ?>" class="left">
-    </a>
-</div>
-    <div class="navmenu">
- <?php      
-        wp_nav_menu(array(
-            'theme_location' => 'primary',
-            'link_before' => '<span class="link-class">',
-            'link_after' => '</span>'
-        ));
-        ?>
-
-</div>
-</div>
+    <div class="container">
+        <div class="menu-toggle">
+            <button class="menu-button" id="menu-button">&#9776;</button>
+        </div>
+        <div class="img">
+            <a href="<?php echo get_home_url(); ?>">
+                <img src="<?php echo get_stylesheet_directory_uri() . '/img/foto1.svg'; ?>" class="left">
+            </a>
+        </div>
+        <div class="navmenu">
+            <?php      
+            wp_nav_menu(array(
+                'theme_location' => 'primary',
+                'link_before' => '<span class="link-class">',
+                'link_after' => '</span>'
+            ));
+            ?>
+        </div>
+    </div>
 </section>
+
+
+<script>
+document.getElementById("menu-button").addEventListener("click", function() {
+    var navmenu = document.querySelector(".navmenu");
+    navmenu.classList.toggle("active");
+});
+
+
+
+
+</script>
+
 
 
